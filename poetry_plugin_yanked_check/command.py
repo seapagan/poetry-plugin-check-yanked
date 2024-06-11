a0677477b5465b8eb2d2d8bfe28856d133819a21
Grant Ramsay <seapagan@gmail.com>
2024-06-11 13:34:13 +0100

    refactor command logic to separate file

---
+++ poetry_plugin_yanked_check/command.py
@@ -0,0 +1,87 @@
+"""Define the 'check-yanked' command."""
+
+from pathlib import Path
+
+import requests
+import rtoml
+from poetry.console.commands.command import Command
+
+from poetry_plugin_yanked_check.status import HTTP_200_OK
+
+
+class YankedCheckerCommand(Command):
+    """Define the 'check-yanked' command."""
+
+    name = "check-yanked"
+    description = "Check for yanked packages in the poetry.lock file"
+
+    def handle(self) -> int:
+        """Handle the command."""
+        lockfile_path = Path("poetry.lock")
+        yanked_packages = self.get_yanked_packages(lockfile_path)
+
+        if yanked_packages:
+            self.line("\n<fg=red>Yanked packages found:</>\n")
+            for name, version, reason in yanked_packages:
+                self.line_error(
+                    f'-> "<fg=red>{name}</>" version {version} '
+                    f"(<fg=yellow>{reason}</>)"
+                )
+            return 1
+
+        self.line("\n<fg=green>No yanked packages found.")
+        return 0
+
+    def get_yanked_packages(
+        self, lockfile_path: Path
+    ) -> list[tuple[str, str, str]]:
+        """Return a list of the yanked packages in the lockfile.
+
+        Returns a list of tuples, where each tuple contains the name, version
+        and reason. If no yanked packages are found, an empty list is returned.
+        """
+        with lockfile_path.open() as file:
+            lock_data = rtoml.load(file)
+
+        yanked_packages = []
+        timeout_seconds = 10
+
+        self.info(
+            f"\nChecking <fg=green>{len(lock_data['package'])}</> "
+            "packages in poetry.lock"
+        )
+
+        for package in lock_data["package"]:
+            name = package["name"]
+            version = package["version"]
+
+            try:
+                response = requests.get(
+                    f"https://pypi.org/pypi/{name}/{version}/json",
+                    timeout=timeout_seconds,
+                )
+
+                if response.status_code == HTTP_200_OK:
+                    package_info = response.json()
+                    yanked = package_info["info"].get("yanked", False)
+
+                    if yanked:
+                        yanked_reason = package_info["info"].get(
+                            "yanked_reason"
+                        )
+                        yanked_packages.append((name, version, yanked_reason))
+                else:
+                    self.line_error(
+                        f"Error fetching data for {name}=={version}: "
+                        f"HTTP {response.status_code}"
+                    )
+
+            except requests.Timeout:
+                self.line_error(
+                    f"Request for {name}=={version} timed out "
+                    "after {timeout_seconds} seconds"
+                )
+            except requests.RequestException as e:
+                self.line_error(f"Request for {name}=={version} failed: {e}")
+
+        return yanked_packages
--- poetry_plugin_yanked_check/main.py
+++ poetry_plugin_yanked_check/main.py
@@ -4,79 +4,10 @@ This plugin will check through your 'poetry.lock' and ensure that there are no
 'Yanked' packages in there.
 """
 
-from pathlib import Path
-
-import requests
-import rtoml
-from poetry.console.commands.command import Command
+from poetry.console.application import Application
 from poetry.plugins.application_plugin import ApplicationPlugin
 
-from poetry_plugin_yanked_check.status import HTTP_200_OK
-
-
-class YankedCheckerCommand(Command):
-    """Define the 'check-yanked' command."""
-
-    name = "check-yanked"
-    description = "Check for yanked packages in the poetry.lock file"
-
-    def handle(self) -> int:
-        """Handle the command."""
-        lockfile_path = Path("poetry.lock")
-        yanked_packages = self.get_yanked_packages(lockfile_path)
-
-        if yanked_packages:
-            self.line("Yanked packages found:")
-            for name, version in yanked_packages:
-                self.line(f"{name}=={version}")
-            return 0
-
-        self.line("No yanked packages found.")
-        return 1
-
-    def get_yanked_packages(self, lockfile_path: Path) -> list[tuple[str, str]]:
-        """Return a list of the yanked packages in the lockfile.
-
-        Returns a list of tuples, where each tuple contains the name and
-        version. If no yanked packages are found, an empty list is returned.
-        """
-        with lockfile_path.open() as file:
-            lock_data = rtoml.load(file)
-
-        yanked_packages = []
-        timeout_seconds = 10
-
-        for package in lock_data["package"]:
-            name = package["name"]
-            version = package["version"]
-
-            try:
-                response = requests.get(
-                    f"https://pypi.org/pypi/{name}/{version}/json",
-                    timeout=timeout_seconds,
-                )
-
-                if response.status_code == HTTP_200_OK:
-                    package_info = response.json()
-                    yanked = package_info["info"].get("yanked", False)
-
-                    if yanked:
-                        yanked_packages.append((name, version))
-                else:
-                    self.line(
-                        f"Error fetching data for {name}=={version}: "
-                        f"HTTP {response.status_code}"
-                    )
-
-            except requests.Timeout:
-                self.line(
-                    f"Request for {name}=={version} timed out "
-                    "after {timeout_seconds} seconds"
-                )
-            except requests.RequestException as e:
-                self.line(f"Request for {name}=={version} failed: {e}")
-
-        return yanked_packages
+from poetry_plugin_yanked_check.command import YankedCheckerCommand
 
 
 def factory() -> YankedCheckerCommand:
@@ -87,7 +18,7 @@ def factory() -> YankedCheckerCommand:
 class YankedCheckerPlugin(ApplicationPlugin):
     """Define the 'yanked-checker' plugin."""
 
-    def activate(self, application: ApplicationPlugin) -> None:
+    def activate(self, application: Application) -> None:
         """Called after the plugin is loaded."""
         application.command_loader.register_factory("check-yanked", factory)
 
