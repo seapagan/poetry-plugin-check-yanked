e6e75d8bbebda43ee9e0846b375bbb23e94a78e1
Grant Ramsay <seapagan@gmail.com>
2024-06-27 12:26:45 +0100

    add test to ensure our class fixture has the correct side-effects
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- tests/test_command.py
+++ tests/test_command.py
@@ -10,26 +10,33 @@ from pyfakefs.fake_filesystem import FakeFilesystem
 from pytest_mock import MockerFixture
 
 
-def test_check_yanked_command_init(
+def test_check_pickledb_init(
     get_fs: FakeFilesystem, mocker: MockerFixture, mock_data_dir: MockerFixture
 ) -> None:
-    """Test the initialization of the CheckYankedCommand class."""
+    """Test that the PickleDB is initialized correctly."""
     mock_pickledb_load = mocker.patch("pickledb.load")
     mock_pickledb_load.return_value = mocker.Mock()
 
-    command = CheckYankedCommand()
+    CheckYankedCommand()
 
-    mock_data_dir.assert_called_once_with(appname="poetry-yanked-check")
-    assert (
-        Path("/mocked/path").exists()
-    )  # This checks if the directory was "created" in the fake file system
     mock_pickledb_load.assert_called_once_with(
         Path("/mocked/path") / "cache.db", False
     )
 
-    assert command.cache is not None
-    assert command.yanked_packages == []
-    assert command.timeout_seconds == DEFAULT_TIMEOUT
 
-    # just a sanity check to ensure that the mock lockfile was created
+def test_instantiate_class(
+    mocker: MockerFixture, yank_class: CheckYankedCommand
+) -> None:
+    """Test that the yank_class fixture returns the CheckYankedCommand class.
+
+    Also ensure that the __init__ side-effects are applied.
+    """
+    assert isinstance(yank_class, CheckYankedCommand)
+
+    assert yank_class.cache is not None
+    assert yank_class.yanked_packages == []
+    assert yank_class._data_dir == Path("/mocked/path")  # noqa: SLF001
+    assert Path("/mocked/path").exists()
     assert Path("./poetry.lock").exists()
+
+    assert yank_class.timeout_seconds == DEFAULT_TIMEOUT
